72bd627b92b3757a1b668013c767a0f721708840
idy <idy0013@gmail.com>
2013-09-17 22:20:17 +0800

    add test of Array.rotate

--- test/00-general.js
+++ test/00-general.js
@@ -1,9 +1,9 @@
 // describe('mongo-ext.js', function() {
-// 	it('should load proper', function() {
-// 		require('../mongo-ext.js');
-// 		$diff.should.be.an.instanceof(Function);
-// 		$equals.should.be.an.instanceof(Function);
-// 	});
+//  it('should load proper', function() {
+//    require('../mongo-ext.js');
+//    $diff.should.be.an.instanceof(Function);
+//    $equals.should.be.an.instanceof(Function);
+//  });
 // });
 
 require('../');
@@ -18,6 +18,35 @@ describe('Array', function() {
     });
     it('should deep flatten [1, [2, [3, [4]]]] as [1, 2, 3, 4]', function() {
       [1, [2, [3, [4]]]].flatten(true).should.eql([1, 2, 3, 4]);
+    });
+  });
+  describe('rotate', function() {
+    it('should rotate [1,2,3,4,5] by -1 as [5,1,2,3,4]', function() {
+      [1, 2, 3, 4, 5].rotate(-1).should.eql([5, 1, 2, 3, 4]);
+    });
+    it('should rotate [1,2,3,4,5] by 1 as [2,3,4,5,1]', function() {
+      [1, 2, 3, 4, 5].rotate(1).should.eql([2, 3, 4, 5, 1]);
+    });
+    it('should rotate [1,2,3,4,5] by 2 as [3,4,5,1,2]', function() {
+      [1, 2, 3, 4, 5].rotate(2).should.eql([3, 4, 5, 1, 2]);
+    });
+    it('should rotate [1,2,3,4,5] by 3 as [4,5,1,2,3]', function() {
+      [1, 2, 3, 4, 5].rotate(3).should.eql([4, 5, 1, 2, 3]);
+    });
+    it('should rotate [1,2,3,4,5] by 4 as [5,1,2,3,4]', function() {
+      [1, 2, 3, 4, 5].rotate(4).should.eql([5, 1, 2, 3, 4]);
+    });
+    it('should rotate [1,2,3,4,5] by 0 as [1,2,3,4,5]', function() {
+      [1, 2, 3, 4, 5].rotate(0).should.eql([1, 2, 3, 4, 5]);
+    });
+    it('should rotate [1,2,3,4] by 1 as [2,3,4,1]', function() {
+      [1, 2, 3, 4].rotate(1).should.eql([2, 3, 4, 1]);
+    });
+    it('should rotate [1,2,3,4] by 2 as [3,4,1,2]', function() {
+      [1, 2, 3, 4].rotate(2).should.eql([3, 4, 1, 2]);
+    });
+    it('should rotate [1] by 0 as [1]', function() {
+      [1].rotate(0).should.eql([1]);
     });
   });
 });
